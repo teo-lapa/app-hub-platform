9225533e725a1926a87606dd21c70863fc9e8583
App Hub Developer <developer@apphub.com>
2025-10-07 18:08:40 +0200

    Revert "Configure Next.js for sharp compatibility on Vercel"
    This reverts commit 4974bad7f98de79c94ba3c3a6816e758045539bd.

--- next.config.js
+++ next.config.js
@@ -9,20 +9,6 @@ const nextConfig = {
   images: {
     domains: [],
   },
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@img/sharp-*',
-      ],
-    },
-  },
-  // Configure sharp for serverless
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('sharp');
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
