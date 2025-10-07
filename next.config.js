4974bad7f98de79c94ba3c3a6816e758045539bd
App Hub Developer <developer@apphub.com>
2025-10-07 18:07:19 +0200

    Configure Next.js for sharp compatibility on Vercel

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@img/sharp-*',
+      ],
+    },
+  },
+  // Configure sharp for serverless
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('sharp');
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
